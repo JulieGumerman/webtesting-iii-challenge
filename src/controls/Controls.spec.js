625ca6e29d4bc9d35fa1531cf31f52effd8e5fd9
bodhibrdr <julie.gumerman@gmail.com>
2019-09-16 14:05:36 -0600

    outline for all tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,7 +1,7 @@
 // Test away!
 
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Controls from "./Controls";
 
 
@@ -10,5 +10,18 @@ test("should show buttons that toggle state",() => {
 })
 
 test("text on buttons toggles to reflect state", () => {
-    
+
+})
+
+test("if gate is locked, close button is disabled", () => {
+    const { getByTestId } = render(<Controls />);
+    const lockButton = getByTestId(/lock/i);
+    const openButton = getByTestId(/open/i);
+
+    fireEvent.click(lockButton);
+    expect(openButton.disabled).toEqual(true);
+}) 
+
+test ("locked button is disabled when gate is open", () => {
+
 })
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,27 @@
 // Test away!
+import React from "react";
+import Display from "./Display";
+
+test("defaults to unlocked and open", () => {
+    
+})
+
+test("displays if gate is open/closed and locked/unlocked", () => {
+
+})
+
+test("displays closed if closed prop is true and open otherwise", () =>{
+
+})
+
+test("displays locked if locked prop is true and unlocked otherwise", () => {
+
+})
+
+test("when locked or closed, use red-led class", () => {
+
+})
+
+test("when unlocked or open, use green-led class", () => {
+
+})
